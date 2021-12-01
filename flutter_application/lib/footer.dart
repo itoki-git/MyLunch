a60a94c7748c7cf29e598afd66d6f58a55bad555
itoki-git <zxxgxdfggd@gmail.com>
2021-12-01 20:04:53 +0900

    フッター

---
+++ flutter_application/lib/footer.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class Footer extends StatefulWidget {
+  const Footer();
+
+  @override
+  _Footer createState() => _Footer();
+}
+
+class _Footer extends State<Footer> {
+  int _selectedIndex = 0;
+  final _bottomNavigationBarItems = <BottomNavigationBarItem>[];
+
+  // アイコン情報
+  static const _footerIcons = [
+    Icons.home,
+    Icons.search,
+    Icons.map,
+    Icons.history,
+  ];
+
+  // アイコン文字列
+  static const _footerItemNames = ['ホーム', '検索', '履歴', 'MAP'];
+
+  // 遷移先リスト
+
+  static const _footerRoots = [
+    '/search',
+    '/search',
+    '/search',
+    '/search',
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _bottomNavigationBarItems.add(_UpdateActiveState(0));
+    for (var i = 1; i < _footerItemNames.length; i++) {
+      _bottomNavigationBarItems.add(_UpdateDeactiveState(i));
+    }
+  }
+
+  /// インデックスのアイテムをアクティベートする
+  BottomNavigationBarItem _UpdateActiveState(int index) {
+    return BottomNavigationBarItem(
+      icon: Icon(
+        _footerIcons[index],
+        color: Colors.black87,
+      ),
+      label: _footerItemNames[index],
+    );
+  }
+
+  /// インデックスのアイテムをディアクティベートする
+  BottomNavigationBarItem _UpdateDeactiveState(int index) {
+    return BottomNavigationBarItem(
+      icon: Icon(
+        _footerIcons[index],
+        color: Colors.black26,
+      ),
+      label: _footerItemNames[index],
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _bottomNavigationBarItems[_selectedIndex] =
+          _UpdateDeactiveState(_selectedIndex);
+      _bottomNavigationBarItems[index] = _UpdateActiveState(index);
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed, // これを書かないと3つまでしか表示されない
+      items: _bottomNavigationBarItems,
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
+    );
+  }
+}
