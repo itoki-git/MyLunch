fce8392964350e310a32f07a581b6fabf0fa3261
itoki-git <zxxgxdfggd@gmail.com>
2021-12-01 20:04:58 +0900

    ヘッダー

---
+++ flutter_application/lib/header.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget with PreferredSizeWidget {
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text('ホーム'),
+      elevation: 0,
+    );
+  }
+}
